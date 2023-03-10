64b3724622cbebbc90e072315f0416233fc5aaf4
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2023-03-10 14:30:42 +0100

    variablen

---
+++ JS/Strings/script.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 8;
+
+let result = a + b;
+
+console.log(result);
+
+let vorname = "Luca";
+let nachname = "Mangeng";
+console.log("Mein Name lautet " + vorname + " " + nachname);
+
