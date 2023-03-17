3309e6dccc49e4f9d449c88e30cf6802241890f3
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2023-03-17 14:01:54 +0100

    uebung 7 und 8

---
+++ JS/8. Switch Statement/script.js
@@ -0,0 +1,22 @@
+let random = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+
+
+console.log(random);
+
+switch (random) {
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("six");
+        break;
+    case 7:
+        console.log("seven");
+        break;
+    case 8:
+        console.log("eight");
+        break;
+    case 9:
+        console.log("nine");
+        break;
+}
