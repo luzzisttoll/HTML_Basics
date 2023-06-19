7b25a7f8db5753bc183995ece180012ea9e96249
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2023-06-20 00:11:51 +0200

    finished api calls

--- 7. eigenes Projekt/my-app/src/App.js
+++ 7. eigenes Projekt/my-app/src/App.js
@@ -12,17 +12,20 @@ const App = () => {
 
   return (
     <div>
-        <div className='background-container'>
-          <ScrollHandler onScroll={handleScroll} />
-          <div className={`text-container ${scrollPosition > 270 ? 'fixed' : ''}`}>
-            <div className='title'>Die Workouthilfe</div>
-          </div>
+      <div className='background-container'>
+        <ScrollHandler onScroll={handleScroll} />
+        <div className={`text-container ${scrollPosition > 200 ? 'fixed' : ''}`}>
+          <div className='title'>Die Workouthilfe</div>
         </div>
+      </div>
+      <div className='values'>
+        <div className='text'>Unterhalb kann ausgewählt werden, für welchen Muskel Übungen angezeigt werden sollen. In dem <b>Filter-Menü</b>, kann nach dem Schwierigkeitslevel gefiltert werden</div>
         <div className='search'>
           <ExerciseSearch />
         </div>
+      </div>
       <div className='text'>
-      <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
+        <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
         <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
         <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
         <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
--- 7. eigenes Projekt/my-app/src/components/ExerciseSearch.js
+++ 7. eigenes Projekt/my-app/src/components/ExerciseSearch.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import '../styles/style.css'
 
 function App() {
   const [data, setData] = useState([]);
-  
+
   useEffect(() => {
-    
+
   }, []);
 
   const handleSearch = event => {
@@ -12,34 +13,51 @@ function App() {
     if (!searchTerm) {
       return
     }
-      console.log('https://api.api-ninjas.com/v1/exercises?muscle=' + searchTerm)
-      fetch('https://api.api-ninjas.com/v1/exercises?muscle=' + searchTerm, {
-        headers: {
-          'X-Api-Key': '0OYDidzDI/O9S2U8efeDSA==vfqX55xOhxT87DFp'
-        }
+    console.log('https://api.api-ninjas.com/v1/exercises?muscle=' + searchTerm)
+    fetch('https://api.api-ninjas.com/v1/exercises?muscle=' + searchTerm, {
+      headers: {
+        'X-Api-Key': '0OYDidzDI/O9S2U8efeDSA==vfqX55xOhxT87DFp'
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        setData(json)
       })
-        .then(response => response.json())
-        .then(json => {
-          setData(json)
-        })
-        .catch(error => console.error(error));
+      .catch(error => console.error(error));
   };
 
   return (
     <div>
-      <select onChange={handleSearch}>
-        <option>Please select...</option>
-        <option value={"triceps"}>Triceps</option>
-        <option value={"biceps"}>Bizeps</option>
-      </select>
+      <div className='select'>
+        <select className='custom-select' onChange={handleSearch} required>
+          <option>Select Muscle...</option>
+          <option value='abdominals'>Abdominals</option>
+          <option value='abductors'>Abductors</option>
+          <option value='adductors'>Adductors</option>
+          <option value='biceps'>Biceps</option>
+          <option value='calves'>Calves</option>
+          <option value='chest'>Chest</option>
+          <option value='forearms'>Forearms</option>
+          <option value='glutes'>Glutes</option>
+          <option value='hamstrings'>Hamstrings</option>
+          <option value='lats'>Lats</option>
+          <option value='lower_back'>Lower Back</option>
+          <option value='middle_back'>Middle Back</option>
+          <option value='neck'>Neck</option>
+          <option value='quadriceps'>Quadriceps</option>
+          <option value='traps'>Traps</option>
+          <option value='triceps'>Triceps</option>
+        </select>
+        <span className='select-icon'></span>
+      </div>
       {data.length > 0 ? (
-        <ul>
-          {data.map(exercise => (
-            <li key={exercise.id}>{exercise.name} + {exercise.difficulty} + {exercise.muscle}</li>
-          ))}
-        </ul>
+          <ul>
+            {data.map(exercise => (
+              <p key={exercise.id}>{exercise.name} - {exercise.muscle}</p>
+            ))}
+          </ul>
       ) : (
-        'No exercises found.'
+        '  Choose a muscle to display the exercise.'
       )}
     </div>
   );
