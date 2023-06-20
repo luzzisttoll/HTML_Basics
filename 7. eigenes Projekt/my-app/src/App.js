6998cce16d97df0e3cc7af2d2aaf2b645d831d16
luzzisttoll <91245180+luzzisttoll@users.noreply.github.com>
2023-06-20 09:25:22 +0200

    popup

--- 7. eigenes Projekt/my-app/src/App.js
+++ 7. eigenes Projekt/my-app/src/App.js
@@ -24,13 +24,6 @@ const App = () => {
           <ExerciseSearch />
         </div>
       </div>
-      <div className='text'>
-        <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
-        <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
-        <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
-        <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
-        <p>Est amet cupidatat laborum pariatur sint minim enim esse elit elit eiusmod. <br></br>Proident eu nulla velit laboris. <br></br>Laboris do minim incididunt dolore amet. <br></br>Duis cupidatat et enim ut dolor elit in quis incididunt exercitation id. <br></br>Voluptate occaecat amet culpa anim exercitation nisi nisi ea nisi officia nostrud est incididunt consequat. <br></br>Elit Lorem aliquip veniam non occaecat commodo enim tempor nostrud.</p>
-      </div>
     </div>
   );
 };
--- 7. eigenes Projekt/my-app/src/components/ExerciseSearch.js
+++ 7. eigenes Projekt/my-app/src/components/ExerciseSearch.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/style.css'
+import '../styles/style.css';
 
 function App() {
   const [data, setData] = useState([]);
+  const [selectedExerciseIsOpen, setSelectedExerciseIsOpen] = useState(null);
 
   useEffect(() => {
 
   }, []);
 
   const handleSearch = event => {
-    const searchTerm = event.target.value
+    const searchTerm = event.target.value;
     if (!searchTerm) {
-      return
+      return;
     }
-    console.log('https://api.api-ninjas.com/v1/exercises?muscle=' + searchTerm)
+
     fetch('https://api.api-ninjas.com/v1/exercises?muscle=' + searchTerm, {
       headers: {
         'X-Api-Key': '0OYDidzDI/O9S2U8efeDSA==vfqX55xOhxT87DFp'
@@ -21,16 +22,24 @@ function App() {
     })
       .then(response => response.json())
       .then(json => {
-        setData(json)
+        setData(json);
       })
       .catch(error => console.error(error));
+  };
+
+  const openPopup = exercise => {
+    setSelectedExerciseIsOpen(exercise);
+  };
+
+  const closePopup = () => {
+    setSelectedExerciseIsOpen(null);
   };
 
   return (
     <div>
       <div className='select'>
         <select className='custom-select' onChange={handleSearch} required>
-          <option>Select Muscle...</option>
+          <option selected disabled>Select Muscle...</option>
           <option value='abdominals'>Abdominals</option>
           <option value='abductors'>Abductors</option>
           <option value='adductors'>Adductors</option>
@@ -50,14 +59,29 @@ function App() {
         </select>
         <span className='select-icon'></span>
       </div>
+
       {data.length > 0 ? (
-          <ul>
-            {data.map(exercise => (
-              <p key={exercise.id}>{exercise.name} - {exercise.muscle}</p>
-            ))}
-          </ul>
+        <ul>
+          {data.map(exercise => (
+            <span key={exercise.id}>
+              <p>{exercise.name}</p>
+              <button onClick={() => openPopup(exercise)}>Open Popup</button>
+            </span>
+          ))}
+        </ul>
       ) : (
-        '  Choose a muscle to display the exercise.'
+        <p>Choose a muscle to display the exercises.</p>
+      )}
+
+      {selectedExerciseIsOpen && (
+        <div className='popup'>
+          <div className='popup-content'>
+            <h2>{selectedExerciseIsOpen.name}</h2>
+            <p>{selectedExerciseIsOpen.type} + {selectedExerciseIsOpen.muscle} + {selectedExerciseIsOpen.difficulty}</p>
+            <p>{selectedExerciseIsOpen.instructions}</p>
+            <button onClick={closePopup}>Close</button>
+          </div>
+        </div>
       )}
     </div>
   );
